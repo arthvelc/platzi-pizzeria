ab7ea09302ead5a22351105770a3d82b67ff6360
Arturo Solares <asolares.devml@gmail.com>
2024-11-13 12:58:31 -0600

    first commit

---
+++ src/main/java/com/platzi/pizza/PlatziPezzeriaApplication.java
@@ -0,0 +1,14 @@
+package com.platzi.pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatziPezzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatziPezzeriaApplication.class, args);
+
+	}
+
+}
